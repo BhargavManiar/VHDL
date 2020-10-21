1038102deb64f2fd8dd001be4409a492b788da90
ec18096 <b.maniar@se18.qmul.ac.uk>
2020-10-21 23:20:00 +0100

    Completed RE_DFF (Rising Edge DFF)

--- DTypeFlipFlops/RisingEdge/RE_DFF/RE_DFF.srcs/sources_1/new/RE_DFF.vhd
+++ DTypeFlipFlops/RisingEdge/RE_DFF/RE_DFF.srcs/sources_1/new/RE_DFF.vhd
@@ -3,12 +3,23 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 
 entity RE_DFF is
---  Port ( );
+    port(
+    Q, notQ : out std_logic; 
+    CLK, D : in std_logic
+    );
+
 end RE_DFF;
 
 architecture Behavioral of RE_DFF is
 
 begin
 
-
+    process(CLK)
+    begin
+        
+        if(rising_edge(CLK)) then
+            Q <= D;
+        end if; 
+    end process; 
+ 
 end Behavioral;
